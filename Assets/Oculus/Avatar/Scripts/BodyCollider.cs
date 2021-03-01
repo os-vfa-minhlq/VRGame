4a7f9f8e530c7e1af455c74287ba331559d726b4
minhlq <minhlq.onetech@gmail.com>
2021-03-02 02:50:26 +0700

    Add prototype

---
+++ Assets/Oculus/Avatar/Scripts/BodyCollider.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BodyCollider : MonoBehaviour
+{
+    public OvrAvatar owner = null;
+    private Vector3 oldpos = default;
+    private Vector3 newpos = default;
+    private Rigidbody rigidBody = default;
+
+    private void Start()
+    {
+        oldpos = transform.position;
+        rigidBody = this.GetComponent<Rigidbody>();
+    }
+    
+    private void Update()
+    {
+        newpos = transform.position;
+        var media =  (newpos - oldpos);
+        rigidBody.velocity = media /Time.deltaTime;
+        oldpos = newpos;
+        newpos = transform.position;
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.transform.tag != this.transform.tag)
+        {
+            owner.TakeDamage(other.relativeVelocity.magnitude/2);
+        }
+
+        
+    }
+
+    
+
+}
---
+++ Assets/Oculus/Avatar/Scripts/HandCollider.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandCollider : MonoBehaviour
+{
+    public OvrAvatar owner = null;
+    private Vector3 oldpos = default;
+    private Vector3 newpos = default;
+    private Rigidbody rigidBody = default;
+    
+    
+    private void Start()
+    {
+        oldpos = transform.position;
+        rigidBody = this.GetComponent<Rigidbody>();
+    }
+    
+    private void Update()
+    {
+        newpos = transform.position;
+        var media =  (newpos - oldpos);
+        rigidBody.velocity = media /Time.deltaTime;
+        oldpos = newpos;
+        newpos = transform.position;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.GetComponent<HandCollider>() != null)
+        {
+            Debug.Log(this.transform.tag + " something hit hand");
+            this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            oldpos = newpos = transform.position;
+        }
+    }
+
+}
